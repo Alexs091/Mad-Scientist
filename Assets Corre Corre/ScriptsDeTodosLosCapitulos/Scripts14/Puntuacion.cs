3e1fe412c456f94bedd04976ff9e1230212a0ffb
Alejandro Marin Pons <anafralex@hotmail.com>
2015-03-07 10:12:41 +0100

    Zips fuera

---
+++ Assets Corre Corre/ScriptsDeTodosLosCapitulos/Scripts14/Puntuacion.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Puntuacion : MonoBehaviour {
+
+	private int puntuacion = 0;
+
+	// Use this for initialization
+	void Start () {
+		NotificationCenter.DefaultCenter().AddObserver(this, "IncrementarPuntos");
+	}
+
+	void IncrementarPuntos(Notification notificacion){
+		int puntosAIncrementar = (int)notificacion.data;
+		puntuacion+=puntosAIncrementar;
+		Debug.Log("Incrementado "+puntosAIncrementar+" puntos. Total ganados: "+puntuacion);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets Corre Corre/ScriptsDeTodosLosCapitulos/Scripts18/EstadoJuego.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EstadoJuego : MonoBehaviour {
+
+	public int puntuacionMaxima = 0;
+
+	public static EstadoJuego estadoJuego;
+
+	void Awake(){
+		if(estadoJuego==null){
+			estadoJuego = this;
+			DontDestroyOnLoad(gameObject);
+		}else if(estadoJuego!=this){
+			Destroy(gameObject);
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
